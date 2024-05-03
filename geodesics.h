727841bdd142704f5b007784a96d8b00ce8378db
Louis Touzalin <ltouzali@3G4.42angouleme.fr>
2024-05-03 20:25:34 +0200

    lol

---
+++ geodesics.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_POINTS 100000
+#define c 299792458
+#define G 6.67430e-11
+#define M 1.9884e21
+#define a 0.0
+#define ALIGNMENT 32
+#define BLOCK_SIZE 1000
+typedef long double __attribute__((aligned(32))) ldouble_a32;
+
