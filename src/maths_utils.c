c8ffe668120a7e8e146bb4761faded6b937c6b76
at0m741 <louistouz@icloud.com>
2024-07-06 00:21:22 +0200

    Update maths_utils.c
    sqrtpd zmm0 zmm0

--- src/maths_utils.c
+++ src/maths_utils.c
@@ -12,6 +12,10 @@
 
 #include "../headers/geodesics.h"
 
+double _mm256_sqrt_simd_asm(double x) {__asm__ __volatile__("sqrtpd %ymm0, %ymm0");}
+#ifdef __AVX512__
+    double _mm512_sqrt_simd_asm(double x) {__asm__ __volatile__("sqrtpd %zmm0, %zmm0");}
+#endif
 __m256d _mm256_exp_pd(__m256d x) {
     double vals[4];
     _mm256_storeu_pd(vals, x);
@@ -37,14 +41,4 @@ __m256d _mm256_cos_pd(__m256d x) {
         vals[i] = cosf(vals[i]);
     }
     return _mm256_loadu_pd(vals);
-}
-
-double _mm256_sqrt_simd_asm(double x) {
-    double result;
-    __asm__ __volatile__ (
-        "sqrtpd %1, %0\n"
-        : "=x" (result)
-        : "x" (x)
-    );
-    return result;
 }
