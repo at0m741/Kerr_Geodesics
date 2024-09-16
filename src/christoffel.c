5a377a1a962f8f243c7e5a0e885202b540c56825
at0m741 <louistouz@icloud.com>
2024-09-16 11:09:25 +0200

    Update christoffel.c

--- src/christoffel.c
+++ src/christoffel.c
@@ -56,12 +56,12 @@ void christoffel_symbols(double X[NDIM], double h, double gamma[NDIM][NDIM][NDIM
     VEC_TYPE vec_gcon, vec_dgcov_mu_nu_sigma, vec_dgcov_nu_mu_sigma, vec_dgcov_sigma_mu_nu;
     VEC_TYPE vec_sum, vec_half, vec_gamma_lambda_mu_nu;
 
-    vec_half = VEC_SET1_PD(0.5);  // Préparer la constante 0.5 en vecteur AVX
+    vec_half = VEC_SET1_PD(0.5);
 
     for (int lambda = 0; lambda < NDIM; lambda++) {
         for (int mu = 0; mu < NDIM; mu++) {
             for (int nu = 0; nu < NDIM; nu++) {
-                vec_sum = VEC_SET0_PD();  // Initialiser la somme à 0
+                vec_sum = VEC_SET0_PD();
 
                 for (int sigma = 0; sigma < NDIM; sigma++) {
                     vec_gcon = VEC_SET1_PD(_gcon[lambda][sigma]);
@@ -74,11 +74,11 @@ void christoffel_symbols(double X[NDIM], double h, double gamma[NDIM][NDIM][NDIM
                     vec_term = VEC_SUB_PD(vec_term, vec_dgcov_sigma_mu_nu);
                     vec_term = VEC_MUL_PD(vec_gcon, vec_term);
 
-                    vec_sum = VEC_ADD_PD(vec_sum, vec_term);  // Ajouter au vecteur somme
+                    vec_sum = VEC_ADD_PD(vec_sum, vec_term);
                 }
 
-                vec_gamma_lambda_mu_nu = VEC_MUL_PD(vec_sum, vec_half);  // Multiplier par 0.5
-                gamma[lambda][mu][nu] = VEC_EXTRACT_D(vec_gamma_lambda_mu_nu);  // Extraire et stocker
+                vec_gamma_lambda_mu_nu = VEC_MUL_PD(vec_sum, vec_half);
+                gamma[lambda][mu][nu] = VEC_EXTRACT_D(vec_gamma_lambda_mu_nu);
             }
         }
     }
